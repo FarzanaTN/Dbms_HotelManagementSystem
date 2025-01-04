3e48de56269177be12f85ccaea6d67f53c837778
FarzanaTN <nishatfarzana14@gmail.com>
2025-01-04 22:45:07 +0600

    fixing span

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
             <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></spa>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
